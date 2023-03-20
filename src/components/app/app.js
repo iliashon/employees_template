36f2d6ad62156ccdb9227de975413b2ce179fcc7
Ilya Tupalski <itupalski@gmail.com>
2023-03-20 18:36:14 +0400

    feat: add click star (Mon, Mar 20, 2023  6:36:14 PM)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,19 +7,26 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+  const data = [
+    {name: 'Alex C.', salary: 800, increase: false, id: 1},
+    {name: 'Jhon M.', salary: 1000, increase: true, id: 2},
+    {name: 'Carl W.', salary: 5000, increase: false, id: 3},
+  ]
+
+    return (
+      <div className="app">
+          <AppInfo />
+
+          <div className="search-panel">
+              <SearchPanel/>
+              <AppFilter/>
+          </div>
+          
+          <EmployeesList data={data}/>
+          <EmployeesAddForm/>
+      </div>
+    );
 }
 
 export default App;
