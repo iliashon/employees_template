832d0431863cfd93acfd29d534bad7501323dc76
Ilya Tupalski <itupalski@gmail.com>
2023-03-27 17:28:38 +0400

    feat: add function delete employees (Mon, Mar 27, 2023  5:28:37 PM)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,14 +8,32 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       data: [
+        {name: 'Alex C.', salary: 800, increase: false, id: 1},
+        {name: 'Jhon M.', salary: 1000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3},
+      ]
+    }
+  }
+  
+  deleteitem = (id) => {
+    this.setState(({data}) => {
+      // const index = data.findIndex(elem => elem.id === id);
+      // const before = data.slice(0, index);
+      // const after = data.slice(index + 1);
+      // const newArr = [...before, ...after]
 
-  const data = [
-    {name: 'Alex C.', salary: 800, increase: false, id: 1},
-    {name: 'Jhon M.', salary: 1000, increase: true, id: 2},
-    {name: 'Carl W.', salary: 5000, increase: false, id: 3},
-  ]
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
 
+  render() {
     return (
       <div className="app">
           <AppInfo />
@@ -23,10 +43,13 @@ function App() {
               <AppFilter/>
           </div>
           
-          <EmployeesList data={data}/>
+          <EmployeesList 
+          data={this.state.data}
+          onDelete={this.deleteitem}/>
           <EmployeesAddForm/>
       </div>
     );
+  }
 }
 
 export default App;
